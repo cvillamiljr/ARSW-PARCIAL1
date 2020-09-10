c910ca007be18089e5303df7aba6c247f16108b0
Cesar Villamil <cesar.villamil@mail.escuelaing.edu.co>
2020-09-10 13:16:36 -0500

    LogicaParte2

--- PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/Concurrent.java
+++ PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/Concurrent.java
@@ -1,2 +1,36 @@
-package edu.eci.arsw.primefinder;public class Concurrent {
+package edu.eci.arsw.primefinder;
+
+import edu.eci.arsw.math.MathUtilities;
+
+import java.math.BigInteger;
+
+public class Concurrent extends Thread {
+
+    private final BigInteger inicio;
+    private final BigInteger fin;
+    private final PrimesResultSet prs;
+
+    public Concurrent (BigInteger _a, BigInteger _b, PrimesResultSet prs) {
+        this.inicio = _a;
+        this.fin = _b;
+        this.prs = prs;
+
+
+    }
+    public void run(){
+
+        MathUtilities mt=new MathUtilities();
+        int itCount=0;
+
+        BigInteger i = inicio;
+        while (i.compareTo(fin)<=0){
+            itCount++;
+            if (mt.isPrime(i)){
+                prs.addPrime(i);
+
+            }
+
+            i=i.add(BigInteger.ONE);
+        }
+    }
 }
--- PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimeFinder.java
+++ PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimeFinder.java
@@ -1,38 +1,43 @@
 package edu.eci.arsw.primefinder;
 
 import edu.eci.arsw.math.MathUtilities;
+
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PrimeFinder{
-        
-	
-	
-        
-	public static void findPrimes(BigInteger _a, BigInteger _b, PrimesResultSet prs){
-            
-                BigInteger a=_a;
-                BigInteger b=_b;
+public class PrimeFinder {
 
-                MathUtilities mt=new MathUtilities();
-                
-                int itCount=0;
-            
-                BigInteger i=a;
-                while (i.compareTo(b)<=0){
-                    itCount++;
-                    if (mt.isPrime(i)){
-                        prs.addPrime(i);
-                    }
 
-                    i=i.add(BigInteger.ONE);
-                }
-                
-	}
-	
-	
-	
-	
-	
+    public static void findPrimes(BigInteger _a, BigInteger _b, PrimesResultSet prs) {
+
+        ArrayList<Concurrent> hilos = new ArrayList<>();
+        BigInteger a = _a;
+        BigInteger b = _b;
+        BigInteger cantidad = new BigInteger("4");
+        BigInteger total = null;
+        total = b.subtract(a).divide(cantidad);
+        int division = Math.round(total.intValue());
+        for (int i = 0; i < 4; i++) {
+            int inicio = (division * i);
+            int fin = (division * (i + 1)) - 1;
+            Concurrent hilo = new Concurrent(new BigInteger(String.valueOf(inicio + a.intValue())), new BigInteger(String.valueOf(fin + a.intValue())), prs);
+            hilos.add(hilo);
+        }
+        for (Concurrent i : hilos) {
+            i.start();
+        }
+        for (Concurrent i : hilos) {
+            try {
+                i.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+
 }
